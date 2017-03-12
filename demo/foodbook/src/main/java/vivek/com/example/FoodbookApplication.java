8124ac45d7a308cac729c8adb2afcea2cd61f411
vivek <vivekjwl@gmail.com>
2017-03-12 18:48:20 +0900

    food recipe backend

---
+++ demo/foodbook/src/main/java/vivek/com/example/FoodbookApplication.java
@@ -0,0 +1,12 @@
+package vivek.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbookApplication {
+
+	public static void main(String[] args) {-
+		SpringApplication.run(FoodbookApplication.class, args);
+	}
+}
