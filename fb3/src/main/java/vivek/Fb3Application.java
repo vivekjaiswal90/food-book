e44b530d907fc07aac8c13a8cdb44e19d6290f60
vivek <vivekjwl@gmail.com>
2017-03-27 01:17:56 +0900

    backend_01

---
+++ fb3/src/main/java/vivek/Fb3Application.java
@@ -0,0 +1,12 @@
+package vivek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fb3Application.class, args);
+	}
+}
